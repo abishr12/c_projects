706d26a232111c20fca62a1ca7c1aecf776d3ff3
abishr-ai <abishr@aaris.ai>
2025-11-26 15:22:47 -0600

    chapter 4

--- com/craftinginterpreters/lox/Lox.java
+++ com/craftinginterpreters/lox/Lox.java
@@ -7,7 +7,6 @@ import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
-import java.util.Scanner;  
 
 public class Lox {
     static boolean hadError = false;
@@ -43,7 +42,7 @@ public class Lox {
 
     private static void run(String source) {
         Scanner scanner = new Scanner(source);
-        List<Token> tonkens = scanner.scanTokens();
+        List<Token> tokens = scanner.scanTokens();
 
         // print tokens
 
