d5026b748a4fb6d1309941edf9eb1ae0e3cb926d
abishr-ai <abishr@aaris.ai>
2025-11-25 18:48:23 -0600

    scanner

---
+++ com/craftinginterpreters/lox/Token.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.lox;
+
+class Token {
+    final TokenType type;
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenType type, String lexeme, Object literal, int line){
+        this.type = type;
+        this.lexeme = lexeme;
+        this.line = line;
+        this.literal = literal;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
